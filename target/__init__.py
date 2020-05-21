8a4f004938a81e816d5801d4d467383f251f4307
admin <parthsharma.asia@gmail.com>
2020-05-22 03:49:41 +0530

    Configuration from object

--- target/__init__.py
+++ target/__init__.py
@@ -2,17 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from target.config import Config
 
 app=Flask(__name__)
-app.secret_key='zR0pu1dXbAOhlC-sRX5SzQ'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///sqlite.db'
+
+app.config.from_object(Config)
+
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "parthsharma.asia@gmail.com"
-app.config['MAIL_PASSWORD'] = "q6w6e6r6t6y"
+
 mail = Mail(app)
 
 from target.users.routes import users
---
+++ target/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY='zR0pu1dXbAOhlC-sRX5SzQ'
+    SQLALCHEMY_DATABASE_URI='sqlite:///sqlite.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "parthsharma.asia@gmail.com"
+    MAIL_PASSWORD = "q6w6e6r6t6y"
